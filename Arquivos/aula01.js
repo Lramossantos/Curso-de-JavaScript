c04bde627a5e24bf09a47739bc6c151e7b0d663c
Leonardo Ramos dos Santos <leonardoramosdossantos92@gmail.com>
2025-09-27 03:41:28 -0300

    Add initial HTML and JS files for Aula 01
    Created aula01.html with basic HTML structure and linked aula01.js. Added introductory JavaScript code with console logs and comments for Aula 01.

---
+++ Arquivos/aula01.js
@@ -0,0 +1,9 @@
+console.log("Aula 01 - Introdução ao JavaScript");
+//console.log("Esta linha esta comentada");
+/*
+console.log("Outra linha comentada");
+console.log("Mais uma linha comentada");
+*/
+
+
+console.log("Hello World!");
